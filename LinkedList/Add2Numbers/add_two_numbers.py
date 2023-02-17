4a1498d12cc10b85990c6fe28ab03a8bf461ab76
Huyen Pham <thanhhuyen2995@gmail.com>
2023-02-17 20:41:15 +0000

    fixed code

--- LinkedList/Add2Numbers/add_two_numbers.py
+++ LinkedList/Add2Numbers/add_two_numbers.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     dummy = ListNode(0)
